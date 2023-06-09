066ee2ec125f3d79ed3949aa8dc86b69c2e3ec4f
KostCheshire <124019167+KostCheshire@users.noreply.github.com>
2023-06-09 17:11:26 +0700

    Add files via upload

---
+++ canalyze.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Функция для поиска функции в файле и вывода ее тела
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s [filename]\n",argv[0]);
+        return 1;
+    }
+
+    char* filename = argv[1];
+    
+    FILE*fp = fopen(filename,"r");
+    if (fp == NULL) {
+    	printf("Error: failed to opened file %s\n",filename);
+    	return 1;
+	}
+	
+	char functionName[256];
+	while (1){
+		printf("Input function:");
+		if (scanf("%s", functionName) != 1)
+		{
+			printf("Failed to read function name, try again\n");
+			continue;
+		}
+		break;
+	}
+	
+	fseek(fp,0,SEEK_END);
+	long fsize = ftell(fp);
+	fseek(fp,0,SEEK_SET);
+	char*buffer = (char*)malloc(fsize + 1);
+	fread(buffer,fsize,1,fp);
+	fclose(fp);
+	buffer[fsize] = '0';
+	
+	int lineNumber = 1;
+	char*line = strtok(buffer, "\n");
+	while (line != NULL) {
+		if (strstr(line,functionName)!= NULL && strstr(line,"(") != NULL && strstr(line,"(") != NULL){
+			printf("%d: %s\n",lineNumber,line);
+		}
+		lineNumber++;
+		line= strtok(NULL,"\n");
+	}
+	free(buffer);
+	return 0;
+}
+
